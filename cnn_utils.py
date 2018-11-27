1f8dcdc91ffe008e18072b65bfc1185f76706d39
David An <dong82@gmail.com>
2018-11-27 16:12:15 -0500

    Commented out unused function

--- cnn_utils.py
+++ cnn_utils.py
@@ -36,27 +36,27 @@ def forward_propagation_for_predict(X, parameters):
     
     return Z3
 
-def enable_ipython_completer():
-    """ Call this from an interactive IPython session to enable tab-completion
-    of group and attribute names.
-    """
-    import sys
-    if 'IPython' in sys.modules:
-        ip_running = False
-        try:
-            from IPython.core.interactiveshell import InteractiveShell
-            ip_running = InteractiveShell.initialized()
-        except ImportError:
-            # support <ipython-0.11
-            from IPython import ipapi as _ipapi
-            ip_running = _ipapi.get() is not None
-        except Exception:
-            pass
-        if ip_running:
-            from . import ipy_completer
-            return ipy_completer.load_ipython_extension()
+# def enable_ipython_completer():
+#     """ Call this from an interactive IPython session to enable tab-completion
+#     of group and attribute names.
+#     """
+#     import sys
+#     if 'IPython' in sys.modules:
+#         ip_running = False
+#         try:
+#             from IPython.core.interactiveshell import InteractiveShell
+#             ip_running = InteractiveShell.initialized()
+#         except ImportError:
+#             # support <ipython-0.11
+#             from IPython import ipapi as _ipapi
+#             ip_running = _ipapi.get() is not None
+#         except Exception:
+#             pass
+#         if ip_running:
+#             from . import ipy_completer
+#             return ipy_completer.load_ipython_extension()
 
-    raise RuntimeError('Completer must be enabled in active ipython session')
+#     raise RuntimeError('Completer must be enabled in active ipython session')
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_signs.h5', "r")
